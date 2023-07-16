48e62876a1ee0de1ff701cecad4ca5a40b470230
Luiz Campedelli <vapeprosper@gmail.com>
2023-07-16 00:05:11 -0300

    so falta o mailer

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,16 +5,16 @@ class TransactionsController < ApplicationController
     @transactions = Transaction.where(batch_id: latest_batch_id)
 
     @anomalies = @transactions.where(status: ['reversed', 'failed', 'backend_reversed'])
-    @anomalous_transactions = @anomalies.group_by_hour(:created_at).sum(:quantity)
+    @anomalous_transactions = @anomalies.group_by_hour_of_day(:created_at).sum(:quantity)
 
     @alert_transactions = @transactions.where(status: 'denied').where.not(id: @anomalies.pluck(:id))
-    @alert_transactions_count = @alert_transactions.group_by_hour(:created_at).sum(:quantity)
+    @alert_transactions_count = @alert_transactions.group_by_hour_of_day(:created_at).sum(:quantity)
 
     @approved_transactions = @transactions.where(status: 'approved')
-    @approved_transactions_count = @approved_transactions.group_by_hour(:created_at).sum(:quantity)
+    @approved_transactions_count = @approved_transactions.group_by_hour_of_day(:created_at).sum(:quantity)
 
     @processing_transactions = Transaction.where(status: 'processing')
-    @processing_transactions_count = @processing_transactions.group_by_hour(:created_at).sum(:quantity)
+    @processing_transactions_count = @processing_transactions.group_by_hour_of_day(:created_at).sum(:quantity)
   end
 
   def create
